0ec14b212fa7650c775a530c0fd9587ba4016951
Omnavneet <omnavneet79@gmail.com>
2025-12-19 20:06:53 +0530

    add timetable dynamodb store

---
+++ backend/src/services/timetable.store.js
@@ -0,0 +1,29 @@
+import { ddb } from "../config/dynamodb";
+import { PutCommand, QueryCommand, } from "@aws-sdk/lib-dynamodb";
+
+const TABLE = "Timetable";
+
+export async function getTimetable(userId) {
+    const res = await ddb.send(
+        new QueryCommand({
+            TableName: TABLE,
+            KeyConditionExpression: "userId = :u",
+            ExpressionAttributeValues: {
+                ":u": userId,
+            },
+        })
+    );
+
+    return res.Items || [];
+}
+
+export async function upsertSlot(slot) {
+    await ddb.send(
+        new PutCommand({
+            TableName: TABLE,
+            Item: slot,
+        })
+    );
+
+    return slot;
+}
